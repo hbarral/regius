98e9eba891a8e4999d58e06cbcd9350f26f46aea
Héctor Barral <hbarral@mail.ru>
2023-09-24 14:22:59 -0400

    ADD basic folder structure on create new app

---
+++ helpers.go
@@ -0,0 +1,17 @@
+package regius
+
+import "os"
+
+func (c *Regius) CreateDirIfNotExist(path string) error {
+	const mode = 0755
+
+	if _, err := os.Stat(path); os.IsNotExist(err) {
+		err := os.Mkdir(path, mode)
+
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
--- regius.go
+++ regius.go
@@ -1,1 +1,37 @@
 package regius
+
+const version = "1.0.0"
+
+type Regius struct {
+	AppName string
+	Debug   bool
+	Version string
+}
+
+func (c *Regius) New(rootPath string) error {
+	pathConfig := initPath{
+		rootPath:    rootPath,
+		folderNames: []string{"handlers", "migratios", "views", "data", "public", "tmp", "logs", "middleware"},
+	}
+
+	err := c.Init(pathConfig)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (c *Regius) Init(p initPath) error {
+	root := p.rootPath
+	for _, path := range p.folderNames {
+		err := c.CreateDirIfNotExist(root + "/" + path)
+
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
