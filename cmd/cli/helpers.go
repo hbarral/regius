d8a4df5553c0ba5ed9168f420b29a8e57e90a966
Héctor Barral <hbarral@mail.ru>
2023-12-11 15:14:44 -0300

    ADD make session to command line utility

--- cmd/cli/helpers.go
+++ cmd/cli/helpers.go
@@ -67,6 +67,7 @@ func showHelp() {
 	make auth              - creates and runs migrations for authentication tables, and creates models and middleware
 	make handler <name>    - creates a stub handler in the handlers directory
 	make model <name>      - creates a new model in the data directory
+	make session           - creates a table in the database as session store
 
   `)
 }
---
+++ cmd/cli/session.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func doSessionTable() error {
+	dbType := reg.DB.DataType
+
+	if dbType == "mariadb" {
+		dbType = "mysql"
+	}
+
+	if dbType == "postgresql" {
+		dbType = "postgres"
+	}
+
+	fileName := fmt.Sprintf("%d_create_sessions_table", time.Now().UnixMicro())
+
+	upFile := reg.RootPath + "/migrations/" + fileName + "." + dbType + ".up.sql"
+	downFile := reg.RootPath + "/migrations/" + fileName + "." + dbType + ".down.sql"
+
+	err := copyFileFromTemplate("templates/migrations/"+dbType+"_session.sql", upFile)
+	if err != nil {
+		exitGracefully(err)
+	}
+
+	err = copyDataToFile([]byte("DROP TABLE sessions"), downFile)
+	if err != nil {
+		exitGracefully(err)
+	}
+
+	err = doMigrate("up", "")
+	if err != nil {
+		exitGracefully(err)
+	}
+
+	return nil
+}
