543a56fe63d0b4065310078fda07900774809dc3
Héctor Barral <hbarral@mail.ru>
2024-01-28 12:14:37 -0300

    UPDATE setup_test to create a badger database

--- cache/setup_test.go
+++ cache/setup_test.go
@@ -9,7 +9,10 @@ import (
 	"github.com/gomodule/redigo/redis"
 )
 
-var testRedisCache RedisCache
+var (
+	testRedisCache  RedisCache
+	testBadgerCache BadgerCache
+)
 
 func TestMain(m *testing.M) {
 	s, err := miniredis.Run()
@@ -31,6 +34,22 @@ func TestMain(m *testing.M) {
 	testRedisCache.Prefix = "test-regius"
 
 	defer testRedisCache.Conn.Close()
+
+	_ = os.RemovalAll("./testdata/tmp/badger")
+
+	if _, err := os.Stat("./testdata/tmp"); os.IsNotExist(err) {
+		err := os.Mkdir("./testdata/tmp", 0755)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+	err = os.Mkdir("./testdata/tmp/badger", 0755)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	db, _ := badger.Open(badger.DefaultOptions("./testdata/tmp/badger"))
+	testBadgerCache.Conn = db
 
 	os.Exit(m.Run())
 }
