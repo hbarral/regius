928a8b7ad79277f33e76620fe94aaece6a0f8ca5
Héctor Barral <hbarral@mail.ru>
2024-06-16 13:23:31 -0400

    ADD List method to WebDAV filesystem

--- filesystems/webdavfilesystem/webdav.go
+++ filesystems/webdavfilesystem/webdav.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"os"
 	"path"
+	"strings"
 
 	"github.com/studio-b12/gowebdav"
 	"gitlab.com/hbarral/regius/filesystems"
@@ -32,6 +33,28 @@ func (w *WebDAV) getCredentials() *gowebdav.Client {
 
 func (w *WebDAV) List(prefix string) ([]filesystems.Listing, error) {
 	var listing []filesystems.Listing
+	client := w.getCredentials()
+	files, err := client.ReadDir(prefix)
+	if err != nil {
+		return listing, err
+	}
+
+	for _, file := range files {
+		if !strings.HasPrefix(file.Name(), ".") {
+			b := float64(file.Size())
+			kb := b / 1024
+			mb := kb / 1024
+			current := filesystems.Listing{
+				LastModified: file.ModTime(),
+				Key:          file.Name(),
+				Size:         mb,
+				IsDir:        file.IsDir(),
+			}
+
+			listing = append(listing, current)
+		}
+	}
+
 	return listing, nil
 }
 
