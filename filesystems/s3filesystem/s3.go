bda8b9bbda29cf9d448197c4c71176360ac74119
Héctor Barral <hbarral@mail.ru>
2024-06-27 23:36:07 -0400

    ADD Get method for s3 file system

--- filesystems/s3filesystem/s3.go
+++ filesystems/s3filesystem/s3.go
@@ -82,6 +82,37 @@ func (s *S3) List(prefix string) ([]filesystems.Listing, error) {
 }
 
 func (s *S3) Get(destination string, items ...string) error {
+	client := s.getCredentials()
+	sess := session.Must(session.NewSession(&aws.Config{
+		Endpoint:    &s.Endpoint,
+		Region:      &s.Region,
+		Credentials: client,
+	}))
+
+	for _, item := range items {
+		err := func() error {
+			file, err := os.Create(fmt.Sprintf("%s/%s", destination, item))
+			if err != nil {
+				return err
+			}
+			defer file.Close()
+
+			downloader := s3manager.NewDownloader(sess)
+			_, err = downloader.Download(file, &s3.GetObjectInput{
+				Bucket: aws.String(s.Bucket),
+				Key:    aws.String(item),
+			})
+			if err != nil {
+				return err
+			}
+
+			return nil
+		}()
+		if err != nil {
+			return err
+		}
+	}
+
 	return nil
 }
 
