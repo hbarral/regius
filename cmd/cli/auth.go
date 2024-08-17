61be3b539db675bef96cb409764f3f1df8f0c6cd
Héctor Barral <hbarral@mail.ru>
2024-08-17 10:57:56 -0400

    UPDATE make auth command

--- cmd/cli/auth.go
+++ cmd/cli/auth.go
@@ -2,33 +2,38 @@ package main
 
 import (
 	"fmt"
-	"time"
 
 	"github.com/fatih/color"
 )
 
 func doAuth() error {
+	checkForDB()
 	dbType := reg.DB.DataType
-	fileName := fmt.Sprintf("%d_create_auth_tables", time.Now().UnixMicro())
-	upFile := reg.RootPath + "/migrations/" + fileName + ".up.sql"
-	downFile := reg.RootPath + "/migrations/" + fileName + ".down.sql"
 
-	err := copyFileFromTemplate("templates/migrations/auth_tables."+dbType+".sql", upFile)
+	tx, err := reg.PopConnect()
+	if err != nil {
+		exitGracefully(err)
+	}
+	defer tx.Close()
+
+	upBytes, err := templateFS.ReadFile(fmt.Sprintf("templates/migrations/auth_tables.%s.sql", dbType))
 	if err != nil {
 		exitGracefully(err)
 	}
 
-	err = copyDataToFile(
-		[]byte(
-			"DROP TABLE IF EXISTS users CASCADE; DROP TABLE IF EXISTS tokens CASCADE; DROP TABLE IF EXISTS remember_tokens;",
-		),
-		downFile,
+	downBytes := []byte(
+		"DROP TABLE IF EXISTS users CASCADE; DROP TABLE IF EXISTS tokens CASCADE; DROP TABLE IF EXISTS remember_tokens;",
 	)
 	if err != nil {
 		exitGracefully(err)
 	}
 
-	err = doMigrate("up", "")
+	err = reg.CreatePopMigration(upBytes, downBytes, "auth", "sql")
+	if err != nil {
+		exitGracefully(err)
+	}
+
+	err = reg.RunPopMigrations(tx)
 	if err != nil {
 		exitGracefully(err)
 	}
